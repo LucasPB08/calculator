29937e5484f20f805ee73dae17b54d0634225c1a
Lucas Pereira Barrios <l.pereirabarrios@student.tudelft.nl>
2023-04-29 21:01:05 +0200

    Add library

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
-#include "iostream"
+#include <wx/wx.h>
 
+class myApp: public wxApp{
+    public:
+        bool OnInit() override;
+};
 
-int main(){
-    std::cout << "Hello World!";
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
 
-    return 0;
-}
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+
+wxIMPLEMENT_APP(myApp);
+
